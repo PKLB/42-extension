02fce69f9abb4cc79c3a2d7f5ab76e5bf2c1e678
Paco Le berre <ple-berr@c2r8p6.42nice.fr>
2022-05-10 14:47:17 +0200

    '

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.querySelectorAll("div.col-lg-4.col-md-6.col-xs-12.fixed-height").forEach((item) => {item.setAttribute("style", "opacity: .8");});
-document.getElementsByClassName("_").insertAdjacentHTML("hello", "<li><a class=\"inactive\" href=\"https://shop.intra.42.fr\"><span class=\"icon-shopping-1\"></span><span class=\"visible-overlay\">Shop</span></a></li>");
+
+var d1 = rootElement.getElementsByClassName('_');
+d1.insertAdjacentHTML('_', '<li><a class="inactive" href="https://shop.intra.42.fr"><span class="icon-shopping-1"></span><span class="visible-overlay">HELLO THERE</span></a></li>');
+
+//document.getElementsByClassName("_").innerHTML += '<li><a class="inactive" href="https://shop.intra.42.fr"><span class="icon-shopping-1"></span><span class="visible-overlay">HELLO THERE</span></a></li>';
 //document.querySelectorAll("div.col-lg-4.col-md-6.col-xs-12.fixed-height").forEach((item) => {item.setAttribute("style", );});
 //document.querySelectorAll("div.col-lg-4.col-md-6.col-xs-12.fixed-height").forEach((item) => {item.setAttribute("style", "-webkit-backdrop-filter: blur(1.1px)");});
