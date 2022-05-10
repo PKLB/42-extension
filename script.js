2fda3ec7f752cc8b3c4c9ecbfdb8d0a072eb1f9d
noalexan <noah.alxdr@gmail.com>
2022-05-10 14:46:35 +0200

    Modifications Of Future

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.querySelectorAll("div.col-lg-4.col-md-6.col-xs-12.fixed-height").forEach((item) => {item.setAttribute("style", "opacity: .8");});
-document.getElementsByClassName("_").insertAdjacentHTML("hello", "<li><a class=\"inactive\" href=\"https://shop.intra.42.fr\"><span class=\"icon-shopping-1\"></span><span class=\"visible-overlay\">Shop</span></a></li>");
-//document.querySelectorAll("div.col-lg-4.col-md-6.col-xs-12.fixed-height").forEach((item) => {item.setAttribute("style", );});
-//document.querySelectorAll("div.col-lg-4.col-md-6.col-xs-12.fixed-height").forEach((item) => {item.setAttribute("style", "-webkit-backdrop-filter: blur(1.1px)");});
+let div = document.querySelectorAll("._")[0];
+var obj = document.createElement("li");
+obj.innerHTML = "<a href=\"https://google.fr\"><span class=\"link_sup-1\"></span><span class=\"visible-overlay\">Profile</span></a>";
+div.append(obj);
+console.log(obj);
+console.log("Done!");
