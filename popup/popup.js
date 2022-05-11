253e0d525b434330645ae66f67217763c1705f9e
noalexan <noah.alxdr@gmail.com>
2022-05-11 14:04:35 +0200

    Hello World

--- popup/popup.js
+++ popup/popup.js
@@ -10,6 +10,8 @@ document.querySelectorAll(".FrameButtons").forEach(function (a) {
 	});
 });
 
+console.log(window.location.origin);
+
 chrome.storage.sync.get("params", ({ params }) => {
 	const links_list = document.getElementById("links_list");
 
@@ -22,13 +24,8 @@ chrome.storage.sync.get("params", ({ params }) => {
 		li.classList.add("list-group-item");
 		li.innerText = link["visible-overlay"];
 		li.append(close);
-		links_list.append(li);
+		links_list.insertBefore(li, links_list.firstChild);
 	});
-
-	var li = document.createElement('li');
-	li.classList.add("list-group-item");
-	li.innerText = "Add";
-	links_list.append(li);
 
 	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {return new bootstrap.Tooltip(tooltipTriggerEl);});
